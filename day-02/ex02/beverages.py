12e2853197d9272332040442f567462e8b056d8b
Omar Taraki <otaraki@student.1337.ma>
2024-08-03 00:58:56 +0100

    ..

--- day-02/ex02/beverages.py
+++ day-02/ex02/beverages.py
@@ -1,5 +1,4 @@
 
-
 class HotBeverage:
     def __init__(self, name = "hot beverage", price = 0.3):
         self.name = name;
@@ -8,6 +7,8 @@ class HotBeverage:
         return "Just some hot water in a cup."
     def __str__(self):
         return f"name: {self.name}\nprice: {self.price:.2f}\ndescription: {self.description()}"
+
+class Coffee: 
 
 prod = HotBeverage()
 print(prod)
