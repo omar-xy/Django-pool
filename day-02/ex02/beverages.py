1831e260ddcb1adb8ca42829768fbc1574d5e3b9
omar-xy <ortaraki@gmail.com>
2024-08-02 19:24:42 +0100

    done ex2

--- day-02/ex02/beverages.py
+++ day-02/ex02/beverages.py
@@ -1,5 +1,3 @@
-
-
 class HotBeverage:
     def __init__(self, name = "hot beverage", price = 0.3):
         self.name = name;
@@ -9,5 +7,30 @@ class HotBeverage:
     def __str__(self):
         return f"name: {self.name}\nprice: {self.price:.2f}\ndescription: {self.description()}"
 
-prod = HotBeverage()
+class Coffee(HotBeverage):
+    def __init__(self):
+        super().__init__(name = "Coffee", price = 0.7)
+    def description(self):
+        # OVERIDE THE ACTUAL FUNC
+        return "A coffee, to stay awake."
+
+class Tea(HotBeverage):
+    def __init__(self):
+        super().__init__(name = "Tea", price = 0.30)
+    def description(self):
+        return "Just some hot water in a cup."
+
+class Chocolate(HotBeverage):
+    def __init__(self):
+        super().__init__(name = "Chocolate", price = 0.50)
+    def description(self):
+        return "Chocolate, sweet chocolate..."
+
+class Cappuccino(HotBeverage):
+    def __init__(self):
+        super().__init__(name = "Cappuccino", price = 0.40)
+    def description(self):
+        return "Un po’ di Italia nella sua tazza!"
+
+prod = Coffee()
 print(prod)
